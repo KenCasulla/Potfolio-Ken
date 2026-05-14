fdbe7725ad1efb6da6f52f2416e571584dc920bf
Ken casulla <ken.revilla57@gmail.com>
2026-05-15 00:20:38 +0800

    deploying frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,15 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://potfolio-ken.onrender.com'
+  ],
+  credentials: true
+}))
+
+
 app.use(express.json())
 
 // Database connection
